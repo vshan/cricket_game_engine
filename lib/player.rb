1ace42d3d71105968cdb61319c5d0a8be571fe81
Vinay Bhat <vinaybhat001@gmail.com>
2014-08-09 18:58:49 +0530

    expanded player and team

--- lib/player.rb
+++ lib/player.rb
@@ -1,3 +1,7 @@
 class Player
+  attr_accessor :player_name, :team_name, :dominant_hand, :bowling_style, :fielding_position, :front_foot_skill, :back_foot_skill, :bowling_skill
 
+  def initialize
+    
+  end
 end
--- lib/team.rb
+++ lib/team.rb
@@ -5,11 +5,11 @@ class Team
   
   def initialize(team)
     @team_id = team
+    @@no_of_players_in_team = 11
+    @@teams = take_options_from_file_return_array("..\data\teams.txt")
+    set_name_of_team(@team_id)
     load(@team_id)
   end
-
-  @@no_of_players_in_team = 11
-  @@teams = take_options_from_file_return_array("..\data\teams.txt")
 
   def set_name_of_team(id)
     @name_of_team = @@teams[id-1]
@@ -18,7 +18,7 @@ class Team
   def load(team)
     @team_players = []
     for i in 0...@@no_of_players_in_team
-      @team_players[i] = Player.new
+      @team_players[i] = Player.new(@name_of_team)
     end
   end
 
